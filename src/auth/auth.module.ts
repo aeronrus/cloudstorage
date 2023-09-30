32b92c7ba3e59d9ba9ea51ec251a9e9429bb73f6
Ruslan Rogalev <rre@m-ppo.ru>
2023-10-01 00:06:05 +0300

    some step passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AuthController],
+  imports: [UsersModule, PassportModule],
+  controllers: [AuthController, LocalStrategy],
   providers: [AuthService],
 })
 export class AuthModule {}
